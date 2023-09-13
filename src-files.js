35b5c09f1238b699faef8542050510cc6a6cc1da
u1f408 <u1f408@users.noreply.github.com>
2023-09-13 09:34:56 +0000

    deploy: 008687c33851022ae54f58906730cd00000faddb

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "palmrs":["",[],["lib.rs"]],\
 "palmrs_conduit_todotxt":["",[],["conduit_todotxt.rs"]],\
 "palmrs_database":["",[["info",[],["category.rs","mod.rs"]],["record",[],["mod.rs","pdb_record.rs"]]],["format.rs","header.rs","lib.rs","time.rs"]],\
 "palmrs_sync":["",[["conduit",[],["mod.rs","within.rs"]]],["lib.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
